558c599d884732455a64ba029e105d713f0b695b
Vedpal Raghav <vedpal.cav@gmail.com>
2026-06-14 20:41:52 +0530

    Wrap App component with CompanyProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  CompanyProvider
+} from "./context/CompanyContext";
+
+const root =
+  ReactDOM.createRoot(
+    document.getElementById("root")
+  );
+
+root.render(
+  <CompanyProvider>
+    <App />
+  </CompanyProvider>
+);
