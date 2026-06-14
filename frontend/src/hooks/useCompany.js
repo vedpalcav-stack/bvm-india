f97991f96d8d89518a3674792a140e5074003db2
Vedpal Raghav <vedpal.cav@gmail.com>
2026-06-14 20:40:48 +0530

    Add useCompany hook to access CompanyContext

--- frontend/src/hooks/useCompany.js
+++ frontend/src/hooks/useCompany.js
@@ -1,1 +1,16 @@
+```jsx
+import {
+  useContext
+} from "react";
+
+import {
+  CompanyContext
+} from "../context/CompanyContext";
+
+export function useCompany() {
+  return useContext(
+    CompanyContext
+  );
+}
+```
 
