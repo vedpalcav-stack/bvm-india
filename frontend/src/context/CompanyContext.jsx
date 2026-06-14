d3e3eb788f5e161d7c8e9b1e67555686a24a2a6d
Vedpal Raghav <vedpal.cav@gmail.com>
2026-06-14 20:39:28 +0530

    Add CompanyContext and CompanyProvider component

--- frontend/src/context/CompanyContext.jsx
+++ frontend/src/context/CompanyContext.jsx
@@ -1,1 +1,28 @@
+```jsx
+import React, {
+  createContext,
+  useState
+} from "react";
+
+export const CompanyContext =
+  createContext();
+
+export function CompanyProvider({
+  children
+}) {
+  const [company, setCompany] =
+    useState(1);
+
+  return (
+    <CompanyContext.Provider
+      value={{
+        company,
+        setCompany
+      }}
+    >
+      {children}
+    </CompanyContext.Provider>
+  );
+}
+```
 
