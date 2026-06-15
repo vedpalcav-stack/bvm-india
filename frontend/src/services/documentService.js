571237d1ed808debacb2ab77ddf08c299d22501c
Vedpal Raghav <vedpal.cav@gmail.com>
2026-06-15 19:58:33 +0530

    Implement DocumentService with CRUD operations

--- frontend/src/services/documentService.js
+++ frontend/src/services/documentService.js
@@ -1,1 +1,25 @@
+import { api } from "../api";
 
+const DocumentService = {
+  getDocuments: () => {
+    return api.get("/api/documents");
+  },
+
+  getDocumentById: (id) => {
+    return api.get(`/api/documents/${id}`);
+  },
+
+  createDocument: (document) => {
+    return api.post("/api/documents", document);
+  },
+
+  updateDocument: (id, document) => {
+    return api.put(`/api/documents/${id}`, document);
+  },
+
+  deleteDocument: (id) => {
+    return api.delete(`/api/documents/${id}`);
+  },
+};
+
+export default DocumentService;
